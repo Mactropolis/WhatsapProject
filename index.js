4ca28f58aa8a3f1bef2de342b480fa010d4a50b7
Daniel Aoyagi <mac_registros@pobox.com>
2024-02-24 10:24:10 -0300

    melhorias index.js

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-const qrcode = require('qrcode-terminal');
-const conversorDeTimestamp = require('./src/utils/conversorDeTimestamp')
-const trataMensagens = require('./src/chat/commons')
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require("qrcode-terminal");
+const conversorDeTimestamp = require("./src/utils/conversorDeTimestamp");
+const trataMensagens = require("./src/chat/commons");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 
 const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: 'yourFolderName',
-        clientId: 'ProjectWhatsapp'
-    })
+  authStrategy: new LocalAuth({
+    dataPath: "yourFolderName",
+    clientId: "ProjectWhatsapp",
+  }),
 });
 
-
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
 
-client.on('message', async (msg) => {
+client.on("message", async (msg) => {
+
+  const comando = trataMensagens.buscaComando(msg.body);
+  const horario = conversorDeTimestamp.converterParaHorario(msg.timestamp);
+  const data = conversorDeTimestamp.converterParaData(msg.timestamp);
+  const device = msg._data.device;
+
+  console.log(`${msg._data.notifyName} / ${msg._data.type} (${data}:${horario})  : ${msg.body}`);
+  if (msg.body.startsWith("!")) {
     console.log(msg);
-    if (msg.body.startsWith('!')) {
-        const comando = trataMensagens.buscaComando(msg.body)
-        const horario = conversorDeTimestamp.converterParaHorario(msg.timestamp);  
-        const data = conversorDeTimestamp.converterParaData(msg.timestamp);      
-         msg.reply(`Seu comando, enviado ${horario} / ${data}, foi *${comando}*`)
-    }
+    msg.reply(
+      `Seu comando, enviado ${horario} / ${data}, foi *${comando}* enviado de *{}`
+    );
+  }
 });
-
 
 client.initialize();
